3c009a369fbad357ea625c2765f8c12e3299a7b1
AkashChaurasiya <akashchaurasiya3055@gmail.com>
2025-04-01 00:05:58 +0530

    javascript

--- Javascript_01/index.js
+++ Javascript_01/index.js
@@ -30,26 +30,34 @@
 //     console.log('E');
 // }
 
-let num=5;
-switch(num){
-    case 1: console.log('A');
-    break;
-    case 2: console.log('B');
-    break;
-    case 3: console.log('C');
-    break;
-    case 4: console.log('D');
-    break;
-    default: console.log('E');
-}
-for(let i=0;i<5;i++)
-{
-    console.log(i);
-}
+//switch statements
+// let num=5;
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     case 4: console.log('D');
+//     break;
+//     default: console.log('E');
+// }
+
+// for(let i=0;i<5;i++)
+// {
+//     console.log(i);
+// }
 
 let x=0;
-while(x>5)
+while(x<5)
 {
     console.log(x);
     x++;
 }
+
+let y=1;
+do{
+    console.log(y);
+    y++;
+} while(y<10)
