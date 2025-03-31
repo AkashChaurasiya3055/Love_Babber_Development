172e2678076cedae5235ec2f53e28e2d70f6ed3b
AkashChaurasiya <akashchaurasiya3055@gmail.com>
2025-03-31 23:42:24 +0530

    javascript_basics

---
+++ Javascript_01/index.js
@@ -0,0 +1,44 @@
+// console.log("Nameste Duniya version 4");
+
+// let b=true;
+// console.log(b);
+
+// const num=12;
+// // num=13;
+
+// let lastName='Babbar';
+// console.log(lastName);
+
+// lastName=3;
+// console.log(lastName);
+
+//if-else statements
+// let marks=98;
+// if(marks>=90){
+//     console.log('A')
+// }
+// else if(marks>=80){
+//     console.log('B');
+// }
+// else if(marks>=70){
+//     console.log('C');
+// }
+// else if(marks>=60){
+//     console.log('D')
+// }
+// else{
+//     console.log('E');
+// }
+
+let num=5;
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+    default: console.log('E');
+}
