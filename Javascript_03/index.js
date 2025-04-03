d858731635e8ad42a7c890306fc98ce115626aea
AkashChaurasiya <akashchaurasiya3055@gmail.com>
2025-04-03 09:54:19 +0530

    javascript_in_css

--- Javascript_03/index.js
+++ Javascript_03/index.js
@@ -1,5 +1,52 @@
-Math.PI
-console.log(Math.PI);
+// Math.PI
+// console.log(Math.PI);
 
-let x=Math.min(1,3,5,6,4);
-console.log(x);
+// let x=Math.min(1,3,5,6,4);
+// console.log(x);
+
+// let lastName='Babbar';
+// let firstName= new String('Love');
+
+// let message=
+// `this is
+
+//  my first
+
+//   Message`;
+
+// // let word= message.split(' ');
+
+// // console.log(word);
+
+// console.log(message);
+
+// let date=new Date();
+// console.log(date);
+
+let  number=[1,4,5,7];
+// console.log(number);
+
+//  number.push(9);
+// console.log(number);
+
+//end --> push
+//beginning --> unshift
+//middle --> splice
+
+//searching -->
+
+// console.log(number);
+// console.log(number.indexOf(7));
+// console.log(number.indexOf(33));
+
+// console.log(number.includes(7));
+
+// console.log(number.indexOf(4,2));
+
+//Searching element using reference
+// let course=[
+//     {no:1, naam:'Love'},
+//     {no:2, naam: 'Akash'}
+// ];
+
+// console.log(course);
