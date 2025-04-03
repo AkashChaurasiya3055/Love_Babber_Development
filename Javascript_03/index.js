e6927e2cf8fd17f59999f57b0f22f3f9a8a8df47
AkashChaurasiya <akashchaurasiya3055@gmail.com>
2025-04-04 00:14:41 +0530

    javascript_04

--- Javascript_03/index.js
+++ Javascript_03/index.js
@@ -23,7 +23,7 @@
 // let date=new Date();
 // console.log(date);
 
-let  number=[1,4,5,7];
+// let  number=[1,4,5,7];
 // console.log(number);
 
 //  number.push(9);
@@ -50,3 +50,87 @@ let  number=[1,4,5,7];
 // ];
 
 // console.log(course);
+
+//Removing element
+
+// let numbers=[1,2,3,4,5,6,7];
+// //end
+// numbers.pop();
+// //beginning
+// numbers.shift();
+
+// console.log(numbers);
+// //middle
+// numbers.splice(3,1);
+
+// console.log(numbers);
+
+//Empty of array
+
+// let numbers=[2,3,4,5,6];
+// // let number=numbers;
+// // console.log(numbers);
+
+// numbers=[];
+// console.log(numbers);
+
+// numbers.length=0;
+// console.log(numbers);
+
+// Combining of array
+// let first=[2,3,4,5];
+// let second=[6,7,8];
+// let combined=first.concat(second);
+// console.log(combined);
+
+// let marks=[10,20,30,40,50,60,70,80];
+// let sliced=marks.slice(2,6);
+// console.log(sliced);
+
+
+//Others method to combing the array element
+// let first=[1,2,3];
+// let second=[4,5,6];
+// let combined=[...first, 'a', false, ...second,'b',true];
+// console.log(combined);
+
+
+
+//  for each loop
+
+// let arr=[10,20,30,40,50];
+// for(let value of arr)
+// {
+//     console.log(value);
+// }
+
+
+//joining two array
+
+// let numbers=[10,20,30,40,50,60];
+// const joined= numbers.join(',');
+// console.log(joined);
+
+//split two array
+//  let message='This is my first message';
+//  let part=message.split(' ');
+//  console.log(part);
+
+//  let joined= part.join('_');
+//  console.log(joined);
+
+//Sorting  arrays
+// let numbers=[40,50,20,30,40];
+// numbers.sort();
+// console.log(numbers);
+// numbers.reverse();
+// console.log(numbers);
+
+//filtering
+// let numbers=[1,2,-2,-4,6];
+// let filtered=numbers.filter(function(value){
+//     return value>=0;
+// });
+// console.log(filtered);
+
+let numbers=
